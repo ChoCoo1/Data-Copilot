6cb4a72d1d4fff74409b8c74dcbe728c58ab8676
ChoCoo1 <1753373709@qq.com>
2024-05-27 22:48:00 +0800

    完整的前端界面

--- backend/djback/copilot/models.py
+++ backend/djback/copilot/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+# 用户表
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=20, blank=True)  # 添加手机号字段
